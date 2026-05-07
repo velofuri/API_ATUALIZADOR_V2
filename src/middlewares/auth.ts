f3c8a54e2cd326894ce9c8b18e53525fbc491daf
Rafael Oliveira <rafael.oliveira@lacteus.com.br>
2026-05-07 16:47:50 -0300

    adicionar verificação de token no middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import { UnauthorizedError } from '../lib/appError.js'
 
 export async function userMiddleware(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify()
   const user = request.user as { role?: string }
   if (!user || user.role !== 'user') {
     throw new UnauthorizedError('Acesso negado para serviços')
