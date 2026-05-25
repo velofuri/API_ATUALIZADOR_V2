c2cad0ceb8b318bfbc9bb47d67b4811d194f8a2d
Rafael Oliveira <rafael.oliveira@lacteus.com.br>
2026-05-25 16:52:05 -0300

    Corrigir envSchema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   SECRET_KEY: z.string(),
   NODE_ENV: z.string(),
-  COOKIE_SECURE: z.boolean(),
+  COOKIE_SECURE: z.coerce.boolean(),
 })
 
 export const env = envSchema.parse(process.env)
